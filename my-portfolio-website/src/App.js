ccc79fd55bca3b50ef9a1203d99185b41f2b94cc
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-05 00:23:11 +0530

    new changes

--- my-portfolio-website/src/App.js
+++ my-portfolio-website/src/App.js
@@ -1,30 +1,18 @@
-import { Route } from "react-router-dom"
-import { Switch } from "react-router-dom"
-import { ThemeProvider } from "styled-components"
-import { lightTheme } from "./components/Themes"
-import GlobalStyle from "./globalStyles"
-
-//Components
-import Main from "./components/Main"
-import AboutPage from "./components/AboutPage"
-import WorkPage from "./components/WorkPage"
-import MySkillsPage from "./components/MySkillsPage"
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return <>
-    <GlobalStyle />
-    <ThemeProvider theme={lightTheme}>
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/work" component={WorkPage} />
-      <Route exact path="/skills" component={MySkillsPage} />
-    </Switch>
-    </ThemeProvider>
-    </>
-    
+  return (
+    <div className="App">
+      <div className="App-header">a
+      </div>
+      <div className="App-body">
+        b
+      </div>
+      <div className="App-footer">c
+      </div>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
