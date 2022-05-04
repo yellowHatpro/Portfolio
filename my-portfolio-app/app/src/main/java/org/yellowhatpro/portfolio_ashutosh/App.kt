50531da1039a576d396556f789c0694e0122d88d
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-05-04 23:57:58 +0530

    project level declaration

--- my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/App.kt
+++ my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/App.kt
@@ -1,6 +1,8 @@
 package org.yellowhatpro.portfolio_ashutosh
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 }
--- my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/ui/MainActivity.kt
+++ my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package org.yellowhatpro.portfolio_ashutosh.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.yellowhatpro.portfolio_ashutosh.R
+import androidx.activity.compose.setContent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent{
+
+        }
+
     }
 }
