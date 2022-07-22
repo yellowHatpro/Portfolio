0be7e25f87f64760bfb7edd8655d72c8d780ff55
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-07-22 10:00:07 +0530

    migration to compose and refractoring

--- my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/ui/MainActivity.kt
+++ my-portfolio-app/app/src/main/java/org/yellowhatpro/portfolio_ashutosh/ui/MainActivity.kt
@@ -1,14 +1,16 @@
 package org.yellowhatpro.portfolio_ashutosh.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.activity.compose.setContent
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-
+            Text(text = "Hello world")
         }
 
     }
