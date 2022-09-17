9b8d18582031999cb3bd54224669b020d784c8ae
Ashutosh Aswal <75999921+yellowHatpro@users.noreply.github.com>
2022-09-17 22:19:43 +0530

    added bottom bar navigation links:

--- my-portfolio-website/src/components/Footer/Footer.js
+++ my-portfolio-website/src/components/Footer/Footer.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
-import './Footer.css'
+import React, { Component } from "react";
+import "./Footer.css";
 
 class Footer extends Component {
-    render() {
-        return(
-            <footer className="footer">
-                <div className="footer-items">
-                    <i className='fab fa-brands fa-github'></i>
-                    <i className='fab fa-brands fa-instagram'></i>
-                    <i className='fab fa-brands fa-linkedin'></i>
-                    <i className='fab fa-brands fa-twitter'></i>
-                </div>
-            </footer>
-        )
-    }
-
+  render() {
+    return (
+      <footer className="footer">
+        <div className="footer-items">
+          <a href="https://github.com/yellowHatpro">
+            {" "}
+            <i className="fab fa-brands fa-github"></i>
+          </a>
+          <a href="https://www.instagram.com/_aashu_aswal_">
+            {" "}
+            <i className="fab fa-brands fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ashutosh-aswal-785101203/">
+            {" "}
+            <i className="fab fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://twitter.com/AshuAswal7">
+            {" "}
+            <i className="fab fa-brands fa-twitter"></i>
+          </a>
+        </div>
+      </footer>
+    );
+  }
 }
 export default Footer;
