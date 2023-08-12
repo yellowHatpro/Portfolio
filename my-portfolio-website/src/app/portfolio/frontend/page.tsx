1123bf2dfc5a5948e4c3c024229e67f52aca2fd7
yellowhatpro <yellowhatpro3119@gmail.com>
2023-08-12 22:27:57 +0530

    smol change,busy with gl

--- my-portfolio-website/src/app/portfolio/frontend/page.tsx
+++ my-portfolio-website/src/app/portfolio/frontend/page.tsx
@@ -5,7 +5,7 @@ const Frontend = () => {
   <div className={"flex flex-col px-2 py-2 min-w-full"}>
       <div className={"text-3xl flex flex-col  bg-red-600 min-w-full justify-center items-center"}>
        <text> Frontend </text>  
-       <text className={"text-2xl"}> Projects I worked on</text>  
+       <text className={"text-2xl"}> Projects I have worked on</text>
       </div>
       <div className={"text-3xl bg-center px-2 py-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4"}>
       <Card name={"Codes-Practice"}/>
