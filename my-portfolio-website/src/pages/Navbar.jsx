c81e7f7af22188362d3e4d243f50e45f9f0127b0
yellowhatpro <yellowhatpro3119@gmail.com>
2023-10-22 15:27:03 +0530

    width issue solved

--- my-portfolio-website/src/pages/Navbar.jsx
+++ my-portfolio-website/src/pages/Navbar.jsx
@@ -1,11 +1,11 @@
 export const Navbar = () => (
-    <div className={"bg-yellow-400 flex justify-between py-4 px-8"}>
+    <div className={"bg-yellow-400 flex flex-row justify-between py-4 px-8"}>
     <text className={""}>yellowhatpro</text>
-        <div className={"child:px-4"}>
-            <text>About me</text>
-            <text>Projects</text>
-            <text>Education</text>
-            <tex>Let us connect</tex>
+        <div className={"child:px-4 flex flex-row"}>
+            <h1>About me</h1>
+            <h1>Projects</h1>
+            <h1>Education</h1>
+            <h1>Let us connect</h1>
         </div>
     </div>
 )
